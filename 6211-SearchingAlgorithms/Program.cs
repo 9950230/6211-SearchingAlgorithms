8d7e8eb023010c1cb19b9aea3c8a0e1e4cf827b4
Brett Griffin <9950230@student.toiohomai.ac.nz>
2019-05-03 13:58:28 +1200

    First commit

---
+++ 6211-SearchingAlgorithms/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6211_SearchingAlgorithms
+{
+    class Program
+    {
+
+        static int[] RandomIntegers;
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            RandomIntegers = new int[100000000];
+
+            for (int i = 0; i < RandomIntegers.Length; i++)
+            {
+                RandomIntegers[i] = random.Next(1000, 1000000);
+            }
+
+            // ~610ms
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("Found: {0}", Search<int>(999)); // Will not find 999
+            sw.Stop();
+            Console.WriteLine("Generic Linear Search: {0}", sw.ElapsedMilliseconds);
+
+            // ~210ms
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            Console.WriteLine("Found: {0}", SearchInt(999)); // Will not find 999
+            sw2.Stop();
+            Console.WriteLine("Int Linear Search: {0}", sw2.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        // Generic Linear Search
+        static bool Search<T>(T toFind)
+        {
+            for (int i = 0; i < RandomIntegers.Length; i++)
+            {
+                if (RandomIntegers[i].Equals(toFind))
+                    return true;
+            }
+            return false;
+        }
+
+        // Integer linear search
+        static bool SearchInt(int toFind)
+        {
+            for (int i = 0; i < RandomIntegers.Length; i++)
+            {
+                if (RandomIntegers[i] == toFind)
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static int[] RandomIntegers;
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            RandomIntegers = new int[10000000];
+
+            for (int i = 0; i < RandomIntegers.Length; i++)
+            {
+                RandomIntegers[i] = random.Next(100, 10000000);
+            }
+
+            Stopwatch swSort = new Stopwatch();
+            swSort.Start();
+            Array.Sort(RandomIntegers);
+            swSort.Stop();
+            Console.WriteLine("Sort: {0}", swSort.ElapsedMilliseconds);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("Found: {0}", BinarySearch(555));
+            sw.Stop();
+            Console.WriteLine("Binary Search: {0}", sw.ElapsedMilliseconds);
+
+            Console.ReadLine();
+
+        }
+
+        static bool BinarySearch(int find)
+        {
+            int mid;
+            int upper = RandomIntegers.Length;
+            int lower = 0;
+
+            while(lower <= upper)
+            {
+                mid = lower + (upper-lower) / 2;
+                if (RandomIntegers[mid] == find)
+                    return true;
+                else if (find < RandomIntegers[mid])
+                    upper = mid - 1;
+                else
+                    lower = mid + 1;
+            }
+
+            return false;
+        }
+    }
+}
